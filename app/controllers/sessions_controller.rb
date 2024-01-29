b49f0cc37538d6664043ecd97c41cfdcc8cea736
AmuzaDongo <dongoamuza@gmail.com>
2024-01-29 14:19:23 +0300

    add alert below the login button

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -19,7 +19,7 @@ class SessionsController < ApplicationController
 
       redirect_to dashboard_index_path, notice: "Signed in successfully"
     else
-      redirect_to sign_in_path(email_hint: params[:email]), alert: "That email or password is incorrect"
+      redirect_to sign_in_path(email_hint: params[:email]), alert: "Your email or password is incorrect"
     end
   end
 
